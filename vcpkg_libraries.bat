761dd9a35360e580f603b25df46523d9785218fa
Alfonso López Ruiz <allopezr@ujaen.es>
2025-01-23 16:00:40 +0100

    Fix installation and CLion code

--- vcpkg_libraries.bat
+++ vcpkg_libraries.bat
@@ -3,4 +3,7 @@ vcpkg install glew:x64-windows
 vcpkg install glm:x64-windows
 vcpkg install assimp:x64-windows
 vcpkg install imgui:x64-windows
+vcpkg install imgui[opengl3-binding]:x64-windows --recurse
+vcpkg install imgui[glfw-binding]:x64-windows --recurse
 vcpkg install imguizmo:x64-windows
+vcpkg install lodepng:x64-windows
