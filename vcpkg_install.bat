4b3bc05304ac8c24d578d5b0e94284f22f02488d
Alfonso López Ruiz <allopezr@ujaen.es>
2025-01-24 12:48:24 +0100

    Update vcpkg_install.bat

--- vcpkg_install.bat
+++ vcpkg_install.bat
@@ -3,6 +3,6 @@ C:
 if not exist "vcpkg" git clone https://github.com/Microsoft/vcpkg.git
 cd vcpkg
 ./bootstrap-vcpkg.bat
-vcpkg.exe integrate install
 setx PATH "C:\vcpkg\;%PATH%"
 setx VCPKG_DEFAULT_TRIPLET "x64-windows"
+vcpkg.exe integrate install
